a076d2cac5f7403a5c19e6bfbfe98d1ea43a21a0
Jewell <jewell.braden@nordstrom.com>
2024-10-22 14:22:28 -0700

    Initial Commit

---
+++ CVSFlickrApp/CVSFlickrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSFlickrAppApp.swift
+//  CVSFlickrApp
+//
+//  Created by dxek on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSFlickrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
